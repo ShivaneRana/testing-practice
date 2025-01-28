26c4e4cf6455f40e6abf1790066b5863a07caa1b
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-01-28 16:55:01 +0530

    caesarCipher added

--- main.js
+++ main.js
@@ -10,7 +10,83 @@ function reverseString(value){
     return value.split("").reverse().join("");
 }
 
+const calculator = {
+    add(a,b){
+        if(typeof a !== "number" || typeof b !== "number"){
+            return "not a number";
+        }
+        return a + b;
+    },
+    sub(a,b){
+        if(typeof a !== "number" || typeof b !== "number"){
+            return "not a number";
+        }
+        return a - b;
+    },
+    mul(a,b){
+        if(typeof a !== "number" || typeof b !== "number"){
+            return "not a number";
+        }
+        return a*b;
+    },
+    div(a,b){
+        if(typeof a !== "number" || typeof b !== "number"){
+            return "not a number";
+        }
+        return a/b;
+    }
+}
+
+function caesarCipher(value,key){
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const temp = value.split("");
+    const result = [];
+
+    //helper function
+    //checks for lower and uppercase character
+    function isLowerCase(char){
+        if(char === char.toLowerCase()){
+            return true;
+        }
+        return false;
+    }
+
+    //helper function
+    // check if a charcter is a letter or not
+    function isLetter(char){
+        return char.toUpperCase() !== char.toLowerCase();
+    }
+    
+    temp.forEach(item => {
+    if(isLetter(item)){
+
+    
+    if(isLowerCase(item)){
+        const index = (item.charCodeAt() + key) - "a".charCodeAt();
+        if(index > 25){
+            result.push(lowerCase[index - lowerCase.length]);
+        }
+        result.push(lowerCase[index]); 
+    }else{
+        const index = (item.charCodeAt() + key) - "A".charCodeAt();
+        if(index > 25){
+            result.push(upperCase[index - upperCase.length]);
+        }
+        result.push(upperCase[index]); 
+        }
+    }else{
+        result.push(item);
+    }
+    })
+
+    return result.join("");
+}
+
+
 module.exports = {
     capitalize,
-    reverseString
+    reverseString,
+    calculator,
+    caesarCipher
 }
