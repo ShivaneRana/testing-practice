f0f3cb4b6a73e4c5c80915ea9a9e0914e0e81390
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-01-28 18:16:52 +0530

    all test uncommented

--- main.test.js
+++ main.test.js
@@ -1,116 +1,116 @@
 const {capitalize,reverseString,calculator,caesarCipher,analyzeArray} = require('./main.js');
 
-// describe("Capitalize function",() => {
+describe("Capitalize function",() => {
     
-//     it("happy path (1)",() => {
-//         expect(capitalize("Beautiful")).toBe("B")
-//     })
+    it("happy path (1)",() => {
+        expect(capitalize("Beautiful")).toBe("B")
+    })
 
-//     it("happy path (2)",() => {
-//         expect(capitalize("Asthama")).toBe("A")
-//     })
+    it("happy path (2)",() => {
+        expect(capitalize("Asthama")).toBe("A")
+    })
 
-//     it("not a string",() => {
-//         expect(capitalize(123)).toBe("not a string");
-//     })
-// })
+    it("not a string",() => {
+        expect(capitalize(123)).toBe("not a string");
+    })
+})
 
-// describe("ReverseString", () => {
-//     it("happy path (1)",() => {
-//         expect(reverseString("Shivane")).toBe("enavihS");
-//     })
+describe("ReverseString", () => {
+    it("happy path (1)",() => {
+        expect(reverseString("Shivane")).toBe("enavihS");
+    })
 
-//     it("happy path (2)",() => {
-//         expect(reverseString("Naman")).toBe("namaN");
-//     })
+    it("happy path (2)",() => {
+        expect(reverseString("Naman")).toBe("namaN");
+    })
 
     
-//     it("not a string",() => {
-//         expect(reverseString(123)).toBe("not a string");
-//     })
-// })
+    it("not a string",() => {
+        expect(reverseString(123)).toBe("not a string");
+    })
+})
 
-// describe("calculator",() => {
+describe("calculator",() => {
 
-//     it("calculator is an object",() => {
-//         expect(typeof calculator).toBe("object");
-//     })
+    it("calculator is an object",() => {
+        expect(typeof calculator).toBe("object");
+    })
 
-//     it("addition (1)",() => {
-//         expect(calculator.add(6,6)).toBe(12);
-//     })
+    it("addition (1)",() => {
+        expect(calculator.add(6,6)).toBe(12);
+    })
 
-//     it("addition (2)",() => {
-//         expect(calculator.add("shivane",6)).toBe("not a number");
-//     })
+    it("addition (2)",() => {
+        expect(calculator.add("shivane",6)).toBe("not a number");
+    })
 
-//     it("subtraction (1)",() => {
-//         expect(calculator.sub(6,6)).toBe(0);
-//     })
+    it("subtraction (1)",() => {
+        expect(calculator.sub(6,6)).toBe(0);
+    })
     
-//     it("subtraction (2)",() => {
-//         expect(calculator.sub(16,6)).toBe(10);
-//     })
+    it("subtraction (2)",() => {
+        expect(calculator.sub(16,6)).toBe(10);
+    })
 
-//     it("subtraction (3)",() => {
-//         expect(calculator.sub("shivane",6)).toBe("not a number");
-//     })
+    it("subtraction (3)",() => {
+        expect(calculator.sub("shivane",6)).toBe("not a number");
+    })
 
-//     it("subtraction (4)",() => {
-//         expect(calculator.sub(16,116)).toBe(-100);
-//     })
+    it("subtraction (4)",() => {
+        expect(calculator.sub(16,116)).toBe(-100);
+    })
 
-//     it("multiplication (1)", () => {
-//         expect(calculator.mul(10,7)).toBe(70);
-//     })
+    it("multiplication (1)", () => {
+        expect(calculator.mul(10,7)).toBe(70);
+    })
 
-//     it("multiplication (2)", () => {
-//         expect(calculator.mul(10,-7)).toBe(-70);
-//     })
+    it("multiplication (2)", () => {
+        expect(calculator.mul(10,-7)).toBe(-70);
+    })
 
-//     it("multiplication (3)", () => {
-//         expect(calculator.mul(10,"uwu")).toBe("not a number");
-//     })
+    it("multiplication (3)", () => {
+        expect(calculator.mul(10,"uwu")).toBe("not a number");
+    })
 
-//     it("division (1)",() => {
-//         expect(calculator.div(10,10)).toBe(1);
-//     })
+    it("division (1)",() => {
+        expect(calculator.div(10,10)).toBe(1);
+    })
 
-//     it("division (2)",() => {
-//         expect(calculator.div(0,10)).toBe(0);
-//     })
+    it("division (2)",() => {
+        expect(calculator.div(0,10)).toBe(0);
+    })
 
-//     it("division (2)",() => {
-//         expect(calculator.div(0,"uwu")).toBe("not a number");
-//     })
-// })
+    it("division (2)",() => {
+        expect(calculator.div(0,"uwu")).toBe("not a number");
+    })
+})
 
-// describe("caesarCipher",() => {
+describe("caesarCipher",() => {
     
-//     it("lower case(1)",() => {
-//         expect(caesarCipher("z",1)).toBe("a")
-//     })
+    it("lower case(1)",() => {
+        expect(caesarCipher("z",1)).toBe("a")
+    })
 
-//     it("lower case(2)",() => {
-//         expect(caesarCipher("xyz",3)).toBe("abc");
-//     })
+    it("lower case(2)",() => {
+        expect(caesarCipher("xyz",3)).toBe("abc");
+    })
 
-//     it("upper case(1)",() => {
-//         expect(caesarCipher("Z",7)).toBe("G")
-//     })
+    it("upper case(1)",() => {
+        expect(caesarCipher("Z",7)).toBe("G")
+    })
 
-//     it("upper case(2)",() => {
-//         expect(caesarCipher("XYZ",2)).toBe("ZAB");
-//     })
+    it("upper case(2)",() => {
+        expect(caesarCipher("XYZ",2)).toBe("ZAB");
+    })
 
-//     it("mixed case(1)",() => {
-//         expect(caesarCipher("ShiVane",1)).toBe("TijWbof");
-//     })
+    it("mixed case(1)",() => {
+        expect(caesarCipher("ShiVane",1)).toBe("TijWbof");
+    })
 
-//     it("check punctuation(1)",() => {
-//         expect(caesarCipher("Hello, World!",3)).toBe("Khoor, Zruog!");
-//     })
-// })
+    it("check punctuation(1)",() => {
+        expect(caesarCipher("Hello, World!",3)).toBe("Khoor, Zruog!");
+    })
+})
 
 describe("analyzeArray",() => {
     it("return object",() => {
